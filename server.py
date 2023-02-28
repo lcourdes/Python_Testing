117dbeabb0af1db0f3d6e54a839568a49f42cb9f
lcourdes <lea.courdes.murphy@gmail.com>
2023-02-28 12:18:51 +0100

    Modify server avoiding purchase without points

--- server.py
+++ server.py
@@ -44,19 +44,21 @@ def create_app(config):
             flash("Something went wrong-please try again")
             return render_template('welcome.html', club=club, competitions=competitions)
 
-
     @app.route('/purchasePlaces', methods=['POST'])
     def purchasePlaces():
         competition = [c for c in competitions if c['name'] == request.form['competition']][0]
         club = [c for c in clubs if c['name'] == request.form['club']][0]
         placesRequired = int(request.form['places'])
+
+        if placesRequired > int(club['points']):
+            flash('You do not have enough points. Try again')
+            return render_template('booking.html', club=club, competition=competition)
+        
         competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
         flash('Great-booking complete!')
         return render_template('welcome.html', club=club, competitions=competitions)
 
-
     # TODO: Add route for points display
-
 
     @app.route('/logout')
     def logout():
