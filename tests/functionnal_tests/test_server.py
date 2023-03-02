49835e3a00372837f99a66c9afa047dc7d7aea7c
lcourdes <lea.courdes.murphy@gmail.com>
2023-03-02 15:54:30 +0100

    add functionnal tests for purchase places

--- tests/functionnal_tests/test_server.py
+++ tests/functionnal_tests/test_server.py
@@ -42,3 +42,64 @@ class MyTest(LiveServerTestCase):
         book_link[2].click()
         element = chrome_driver.find_element(By.TAG_NAME, 'li')
         assert element.text == 'This is a past event. Please choose a future competition.'
+
+    def test_login_book_places_success(self):
+        self.login('test2@test.co')
+        element = chrome_driver.find_element(By.TAG_NAME, 'h2')
+        assert element.text == 'Welcome, test2@test.co'
+
+        book_link = chrome_driver.find_element(By.LINK_TEXT, 'Book Places')
+        book_link.click()
+        element = chrome_driver.find_element(By.TAG_NAME, 'h2')
+        assert element.text == 'name'
+        places = chrome_driver.find_element(By.NAME, 'places')
+        places.send_keys('1')
+        places.send_keys(Keys.ENTER)
+        element = chrome_driver.find_element(By.TAG_NAME, 'li')
+        assert element.text == 'Great-booking complete!'
+
+    def test_login_book_places_no_success_not_enough_club_points(self):
+        self.login('test@test.co')
+        element = chrome_driver.find_element(By.TAG_NAME, 'h2')
+        assert element.text == 'Welcome, test@test.co'
+
+        book_link = chrome_driver.find_element(By.LINK_TEXT, 'Book Places')
+        book_link.click()
+        element = chrome_driver.find_element(By.TAG_NAME, 'h2')
+        assert element.text == 'name'
+        places = chrome_driver.find_element(By.NAME, 'places')
+        places.send_keys('2')
+        places.send_keys(Keys.ENTER)
+        element = chrome_driver.find_element(By.TAG_NAME, 'li')
+        assert element.text == 'You do not have enough points. Try again'
+
+    def test_login_book_places_no_success_more_than_12_points(self):
+        self.login('test2@test.co')
+        element = chrome_driver.find_element(By.TAG_NAME, 'h2')
+        assert element.text == 'Welcome, test2@test.co'
+
+        book_link = chrome_driver.find_elements(By.LINK_TEXT, 'Book Places')
+        book_link[1].click()
+        element = chrome_driver.find_element(By.TAG_NAME, 'h2')
+        assert element.text == 'name2'
+        places = chrome_driver.find_element(By.NAME, 'places')
+        places.send_keys('13')
+        places.send_keys(Keys.ENTER)
+        element = chrome_driver.find_element(By.TAG_NAME, 'li')
+        assert element.text == 'You can\'t purchase more than 12 places.'
+    
+    def test_login_book_places_no_success_more_than_available_places(self):
+        self.login('test2@test.co')
+        element = chrome_driver.find_element(By.TAG_NAME, 'h2')
+        assert element.text == 'Welcome, test2@test.co'
+
+        book_link = chrome_driver.find_element(By.LINK_TEXT, 'Book Places')
+        book_link.click()
+        element = chrome_driver.find_element(By.TAG_NAME, 'h2')
+        assert element.text == 'name'
+        places = chrome_driver.find_element(By.NAME, 'places')
+        places.send_keys('6')
+        places.send_keys(Keys.ENTER)
+        element = chrome_driver.find_element(By.TAG_NAME, 'li')
+        assert element.text == 'You can\'t purchase more than available places.'
+    
